ae938a178ad0966434eac44eb22920cc6e00aa93
David Lindh <Davidslofficial@gmail.com>
2022-05-03 09:56:45 -0400

    Updates

---
+++ precache-manifest.5737602c8893b597db213d8761aee482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4033b81606bed6fe0cfd0e5790273370",
+    "url": "/Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "71e0711ed9353ef0ebec",
+    "url": "/Pomodoro-Timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/Pomodoro-Timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e0711ed9353ef0ebec",
+    "url": "/Pomodoro-Timer/static/js/main.4f5a0c98.chunk.js"
+  },
+  {
+    "revision": "fa519a5d7be288e60c37",
+    "url": "/Pomodoro-Timer/static/js/runtime-main.3e9ec4f7.js"
+  }
+]);
